2f1b6a5612dd01499719454ac7831fb6ebad40cb
Devansh Agrawal <devanshinspace@gmail.com>
2021-04-18 15:37:17 -0400

    removed octovis

--- dependencies/nlopt/build/make_install_nlopt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-DESTDIR=/home/devansh/vna2v_project_ws/src/AE740_FINAL_PROJECT/dependencies/nlopt/build/nlopt_install make install
-
-cp -r /home/devansh/vna2v_project_ws/src/AE740_FINAL_PROJECT/dependencies/nlopt/build/nlopt_install//usr/local/* /home/devansh/vna2v_project_ws/src/AE740_FINAL_PROJECT/dependencies/nlopt/build/devel/
